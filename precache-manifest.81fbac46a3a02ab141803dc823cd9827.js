edf5c89d3178759b9a4f35dffbdf0331bf61bc7e
Lone Cardoso <gilbertosan927@yahoo.com>
2020-06-23 21:30:18 -0300

    primeiro commit

---
+++ precache-manifest.81fbac46a3a02ab141803dc823cd9827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e082ed2f54f1471276ffc5273a3d0c27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2c7d0bf1575818bb61",
+    "url": "/static/css/main.5f5ea49d.chunk.css"
+  },
+  {
+    "revision": "a026f6e5b0868f652e96",
+    "url": "/static/js/2.4e598cf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e598cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2c7d0bf1575818bb61",
+    "url": "/static/js/main.0b1b217d.chunk.js"
+  },
+  {
+    "revision": "e614e25f31eb3ab8aea8",
+    "url": "/static/js/runtime-main.36353bd0.js"
+  }
+]);
